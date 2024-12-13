538965eab6c11ba6d66b66a8bab00c38b9201a94
Facundo Scrollini <121453608+facuscrollini@users.noreply.github.com>
2024-12-13 12:03:31 +0000

    Hasta ejercicio 10 listo

---
+++ app.js
@@ -0,0 +1,27 @@
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound 
+    }
+
+function fromDollarToYen(dolar){
+    let valor = dolar / oneEuroIs.USD;
+    return valor * oneEuroIs.JPY;
+}
+
+function fromEuroToDollar(euro){
+    return euro * oneEuroIs.USD;
+}
+
+function fromYenToPound(yen){
+    let valor = yen / oneEuroIs.JPY;
+    return valor * oneEuroIs.GBP;
+}
+
+function sum(a, b ) {
+    return a + b;
+}
+module.exports = {sum, fromEuroToDollar};
+
+
+
